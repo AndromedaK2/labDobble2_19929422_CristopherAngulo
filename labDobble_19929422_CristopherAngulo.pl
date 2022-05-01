140f7bf4d473d4b4ac4335b6786fbf9f8394ac75
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-05-01 17:46:14 -0400

    [add] isValidOrder

--- labDobble_19929422_CristopherAngulo.pl
+++ labDobble_19929422_CristopherAngulo.pl
@@ -1,44 +1,46 @@
 :- use_module(library(lists)).
 
 cardsSet(Elements,NumberElement,NumberMaxCard,CardsSet,Random).
-createFirstCard(Elements,N,Card):- firstAuxcreateFirstCard(Elements,N,[],0,Card).
 
 
-firstAuxcreateFirstCard(_,N,Card,N,Card):- !.
-firstAuxcreateFirstCard(Elements,N,FirstCard,Count,Card):-
+createFirstCard(Elements,N,Card):- createFirstCardAuxiliar(Elements,N,[],0,Card).
+
+
+createFirstCardAuxiliar(_,N,Card,N,Card):- !.
+createFirstCardAuxiliar(Elements,N,FirstCard,Count,Card):-
     getFirstElement(Elements,Element),
     getTailElements(Elements,TailElements),
     addElementToCard(Element,FirstCard,FinalCard),
     Count1 is Count + 1,
-    firstAuxcreateFirstCard(TailElements,N,FinalCard,Count1,Card).    
-
-
-
+    createFirstCardAuxiliar(TailElements,N,FinalCard,Count1,Card).    
 
 
 
 getFirstElement([Element|_],Element).
 getTailElements([_|Elements],Elements).
 
-
 %CreateNCard(Elements,N,CardsSet):-
    
+isElement(Element):- number(Element); string(Element); atom(Element).
 
-isElement(Element):-
-    number(Element);
-    string(Element);
-    atom(Element).
+isValidOrder(Order):- 
+    isPrime(Order).
 
+divisible(X,Y) :- 0 is X mod Y, !.
+divisible(X,Y) :- X > Y+1, divisible(X, Y+1).
 
+isPrime(2) :- true,!.
+isPrime(X) :- X < 2,!,false.
+isPrime(X) :- not(divisible(X, 2)).
 
 getFirstCard([Card|_],Card).
 getTailCards([_|Cards],Cards).
 
-addElementToCard(Element,Card,CardUpdate):- 
-    isElement(Element),
-    append(Card,[Element],CardUpdate).
+addElementToCard(Element,Card,CardUpdate):- isElement(Element), append(Card,[Element],CardUpdate).
 
 %addCardToCardsSet().
 
-%Example: trace, createFirstCard([1,2,3,4,5,6],4,Card).
+%Example:  
+% createFirstCard([1,2,3,4,5,6],4,Card).
+% createFirstCard([1,"noruega",3,california,5,6],4,Card).
 
