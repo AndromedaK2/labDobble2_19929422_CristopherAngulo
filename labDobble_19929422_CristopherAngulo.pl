3af31a552dd1c3a800431703673f4c92c057988b
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-05-01 15:22:09 -0400

    [add] logic to firstCard with getter and setters

--- labDobble_19929422_CristopherAngulo.pl
+++ labDobble_19929422_CristopherAngulo.pl
@@ -21,16 +21,19 @@ cardsSet
 
 cardsSet(Elements,NumberElement,NumberMaxCard,CardsSet,Random).
 
+
+    
+
+createFirstCard(Elements,N,Card):- firstAuxcreateFirstCard(Elements,N,[],0,Card).
+
 firstAuxcreateFirstCard(_,N,Card,N,Card):- !.
 firstAuxcreateFirstCard(Elements,N,FirstCard,Count,Card):-
     getFirstElement(Elements,Element),
     getTailElements(Elements,TailElements),
-    append([Element],FirstCard,FinalCard),
+    addElementToCard(Element,FirstCard,FinalCard),
     Count1 is Count + 1,
     firstAuxcreateFirstCard(TailElements,N,FinalCard,Count1,Card).    
-    
-createFirstCard(Elements,N,Card):-
-    firstAuxcreateFirstCard(Elements,N,[],0,Card).
+
 
 getFirstElement([Element|_],Element).
 getTailElements([_|Elements],Elements).
@@ -38,5 +41,8 @@ getTailElements([_|Elements],Elements).
 getFirstCard([Card|_],Card).
 getTailCards([_|Cards],Cards).
 
+addElementToCard(Element,Card,CardUpdate):- append(Card,[Element],CardUpdate).
+addCardToCardsSet().
 
+%Example: trace, createFirstCard([1,2,3,4,5,6],4,Card).
 
