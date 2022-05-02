d65b231f1e67f15bbd57ff919dea6b82ac2a61a0
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-05-01 23:59:41 -0400

    Update labDobble_19929422_CristopherAngulo.pl

--- labDobble_19929422_CristopherAngulo.pl
+++ labDobble_19929422_CristopherAngulo.pl
@@ -2,10 +2,11 @@
 
 %Representación Mazo de Cartas:  Elements X NumberElement X NumberMaxCard X Seed X CardsSet
 cardsSet(Elements,NumberElement,NumberMaxCard,Seed,CardsSet):-
-    isValidOrder(NumberElement),
-    createFirstCard(Elements,NumberElement,Card),
-    emptyCardsSet(EmptyCardsSet),
-    addCardToCardsSet(Card,EmptyCardsSet,CardsSet).
+    getOrder(NumberElement,Order),
+    isValidOrder(Order),
+    createFirstCard(Elements,NumberElement,FirstCard),
+    createNCard(Elements,Order,[FirstCard],0,CardsSet).
+    % addCardToCardsSet(Card,EmptyCardsSet,CardsSet).
 
 %Constructor Vacío
 emptyCardsSet([]).
@@ -42,6 +43,8 @@ createNCardAuxiliar(Elements,NCard,N,J,K,FinalNCard):-
 % EXAMPLE: createNCardAuxiliar([1,2,3,4,5,6,7,8,9,10,11,12,13],[1],3,0,0,Card).
 %createNCard([1,2,3,4,5,6,7,8,9,10,11,12,13],3,[],0,Cards).
 
+
+
 %Selectores
 getFirstElement([Element|_],Element).
 getTailElements([_|Elements],Elements).
@@ -58,7 +61,7 @@ cardsSetFindTotalCards(Card,TotalCards):-
 
 %Pertenencia
 isElement(Element):- number(Element); string(Element); atom(Element).
-isValidOrder(Order):- FinalOrder is Order-1, isPrime(FinalOrder).
+isValidOrder(Order):- isPrime(Order).
 isPrime(2) :- true,!.
 isPrime(X) :- X < 2,!,false.
 isPrime(X) :- not(divisible(X, 2)).
@@ -66,8 +69,8 @@ isPrime(X) :- not(divisible(X, 2)).
 %Helper
 divisible(X,Y) :- 0 is X mod Y, !.
 divisible(X,Y) :- X > Y+1, divisible(X, Y+1).
-calculateIndexToNCards(N,J,K,Index):-
-  Index is (N * J) + (K + 1).
+calculateIndexToNCards(N,J,K,Index):- Index is (N * J) + (K + 1).
+%calculateIndexToN2Cards(N,J,K,I,Index):- Index is 
 
 %Modificador
 addElementToCard(Element,Card,CardUpdate):- isElement(Element), append(Card,[Element],CardUpdate).
