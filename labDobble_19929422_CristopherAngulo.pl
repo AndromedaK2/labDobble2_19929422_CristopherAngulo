b495d818381b1c9c9ffdbb2d2b30676362a05961
AndromedaK2 <102329976+AndromedaK2@users.noreply.github.com>
2022-05-01 20:06:34 -0400

    Update labDobble_19929422_CristopherAngulo.pl

--- labDobble_19929422_CristopherAngulo.pl
+++ labDobble_19929422_CristopherAngulo.pl
@@ -1,46 +1,57 @@
 :- use_module(library(lists)).
 
-cardsSet(Elements,NumberElement,NumberMaxCard,CardsSet,Random).
+%Representación Mazo de Cartas:  Elements X NumberElement X NumberMaxCard X Seed X CardsSet
+cardsSet(Elements,NumberElement,NumberMaxCard,Seed,CardsSet):-
+    isValidOrder(NumberElement),
+    createFirstCard(Elements,NumberElement,Card),
+    emptyCardsSet(EmptyCardsSet),
+    addCardToCardsSet(Card,EmptyCardsSet,CardsSet).
 
+%Constructor Vacío
+emptyCardsSet([]).
 
+%Helper
 createFirstCard(Elements,N,Card):- createFirstCardAuxiliar(Elements,N,[],0,Card).
-
-
 createFirstCardAuxiliar(_,N,Card,N,Card):- !.
 createFirstCardAuxiliar(Elements,N,FirstCard,Count,Card):-
     getFirstElement(Elements,Element),
     getTailElements(Elements,TailElements),
     addElementToCard(Element,FirstCard,FinalCard),
     FinalCount is Count + 1,
-    createFirstCardAuxiliar(TailElements,N,FinalCard,FinalCount,Card).    
+    createFirstCardAuxiliar(TailElements,N,FinalCard,FinalCount,Card).
 
 
-
+%Selectores
 getFirstElement([Element|_],Element).
 getTailElements([_|Elements],Elements).
+getOrder(N,Order):- Order is N-1.
+getFirstCard([Card|_],Card).
+getTailCards([_|Cards],Cards).
+
 
 %CreateNCard(Elements,N,CardsSet):-
-   
+
+%Pertenencia
 isElement(Element):- number(Element); string(Element); atom(Element).
-
-isValidOrder(Order):- 
-    isPrime(Order).
-
-divisible(X,Y) :- 0 is X mod Y, !.
-divisible(X,Y) :- X > Y+1, divisible(X, Y+1).
-
+isValidOrder(Order):- FinalOrder is Order-1, isPrime(FinalOrder).
 isPrime(2) :- true,!.
 isPrime(X) :- X < 2,!,false.
 isPrime(X) :- not(divisible(X, 2)).
 
-getFirstCard([Card|_],Card).
-getTailCards([_|Cards],Cards).
+%Helper
+divisible(X,Y) :- 0 is X mod Y, !.
+divisible(X,Y) :- X > Y+1, divisible(X, Y+1).
 
+%Modificador
 addElementToCard(Element,Card,CardUpdate):- isElement(Element), append(Card,[Element],CardUpdate).
+addCardToCardsSet(Card,CardsSet,FinalCardsSet):-
+    append([Card],CardsSet,FinalCardsSet).
 
-%addCardToCardsSet().
-
-%Example:  
+%Examples:
 % createFirstCard([1,2,3,4,5,6],4,Card).
 % createFirstCard([1,"noruega",3,california,5,6],4,Card).
+% cardsSet([1,2,3,4,5,6,7,8,9,10,11,12,13],4,13,2,CardsSet)
+
+
+
 
